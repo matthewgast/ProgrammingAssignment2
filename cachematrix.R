0434ab9593e75f80a5c0dfd07fc9eaf2c1ab9378
matthewgast <mgast@mgast-mac.local>
2015-04-20 21:12:06 -0700

    Handle non-invertible matrices

--- cachematrix.R
+++ cachematrix.R
@@ -2,45 +2,73 @@
 ##
 ## Matthew Gast, 2015
 ##
-## These functions replace standard matrix functions and
-## cache the inverse of a matrix rather than compute it every
-## time.
+## These functions build on standard matrix functions by storing the
+## inverse of a matrix in a list with the matrix itself.
 
 makeCacheMatrix <- function(x = matrix()) {
-## This function creates an object that caches a matrix inverse.
+## This function creates an object containing a matrix and its inverse.
 ##
-## Input:  A matrix object
-## Output: A list containing the matrix and a cached inverse.  If
-##         the matrix is not invertible, the cache is NULL.
-  
-  inv <- NULL
-  
-  setmtx <- function (mtx) {
-    x <<- mtx     # Set matrix value
-    inv <<- NULL  # Clear the cache on setting value
+## Input:  A matrix object.
+## Output: A list containing the the matrix and its cached inverse.
+## If the matrix is not invertible, the cache is NULL.
+    
+  inverse <- NULL
+
+  # Function 1: set the matrix value (and clear the inverse cache)
+  setMatrix <- function (mtx) {
+    x <<- mtx
+    inverse <<- NULL
   }
-  getmtx <- function () x
-  setsolve <- function (solution) inv <<- solution
-  getsolve <- function () inv
+
+  # Function 2: return the matrix
+  getMatrix <- function () x
+
+  # Function 3: set the inverse value
+  setInverse <- function (myInverse) inverse <<- myInverse
+
+  # Function 4: return the inverse value
+  getInverse <- function () inverse
   
   # Construct return vector
-  list (setmtx=setmtx, getmtx=getmtx,
-        setsolve=setsolve,
-        getsolve=getsolve)
+  list (setMatrix=setMatrix,
+        getMatrix=getMatrix,
+        setInverse=setInverse,
+        getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  inv <- x$getsolve()
-  if (!is.null(inv)) {
+## This function returns the inverse of the matrix passed as the first object.
+##
+## Input:  A matrix object created by makeCacheMatrix
+## Output: The function returns the inverse of the matrix.  It first
+## checks the cache to see if the inverse exists, and if not, it
+## calculates the inverse.
+
+  # Retrieve cache value if it exists
+  inverse <- x$getInverse()
+  if (!is.null(inverse)) {
     message("Retrieving cached inverse")
-    return(inv)
+    return(inverse)
   }
-  mtx <- x$getmtx
-  inv <- solve(mtx)
-  x$setsolve(inv)
-  inv
+
+  isInvertible <- function (m) {
+      # The matrix is invertible if solve() returns a matrix object
+      if (class(try(solve(mtx),silent=TRUE)) == "matrix") {
+          return(TRUE)
+      } else {
+          return(FALSE)
+      }
+  }
+  
+  # Otherwise, calculate inverse and cache it
+  mtx <- x$getMatrix()
+  if (isInvertible(mtx)) {
+      inverse <- solve(mtx)
+  } else {
+      message("Non-invertible matrix")
+      inverse <- NULL
+  }
+  x$setInverse(inverse)
+  inverse
 }
